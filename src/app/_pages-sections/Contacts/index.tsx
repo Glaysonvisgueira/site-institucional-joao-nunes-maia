74fa75b735cdbaaf10f2a4ca063b39a6879e23ae
Glayson Visgueira <glayson.visgueira@gestao.gov.br>
2025-07-22 14:06:08 -0300

    configuracao de mixins do sass para responsividade

--- src/app/_pages-sections/Contacts/index.tsx
+++ src/app/_pages-sections/Contacts/index.tsx
@@ -1,7 +1,18 @@
+
+import styles from './index.module.scss';
+
+
 export default function Contacts() {
   return (
-    <>
-      Sessão de contatos
-    </>
+    <section
+      className={`${styles.page}`}
+    >
+      {/* <div
+        className={`${styles.container}`}
+      >
+        Dentro do container
+      </div> */}
+      <h2>CONTATOS</h2>
+    </section>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,14 @@
 import styles from "./page.module.scss";
 
-//UI components
-import { Button } from "@/components/ui/Button";
-
-//Icons
-import { FaHandHoldingHeart } from "react-icons/fa6";
-
 //Sections
+import Homepage from "./_pages-sections/Homepage";
 import Contacts from "./_pages-sections/Contacts";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <Homepage />
       <Contacts />
-      {/* <main className={styles.main}>
-
-        <Button 
-          icon={<FaHandHoldingHeart />}
-          variant="contained"
-        >
-          Como doar?
-        </Button>
-
-        <Button 
-          icon={<FaHandHoldingHeart />}
-          variant="outlined"
-        >
-          Como doar?
-        </Button>
-
-        <Button 
-          icon={<FaHandHoldingHeart />}
-          variant="text"
-        >
-          Como doar?
-        </Button>
-        
-      </main>
-      <footer className={styles.footer}>
-        
-      </footer> */}
     </div>
   );
 }
